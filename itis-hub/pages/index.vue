6ce2cfa8c4bb9c78c30ea5c7696da4f54e5f19cb
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-07-31 01:26:40 +0300

    itial commit

---
+++ itis-hub/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <button @click="showMesage(greeting)" class="greetingButton">
+      Say some greetings
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      greeting: "Let's begin!"
+    }
+  },
+  methods: {
+    showMesage(mesage: string): void {
+      alert(mesage)
+    }
+  }
+})
+</script>
+
+<style>
+  .container {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+
+  .greetingButton {
+    width: 200px;
+    height: 50px;
+    border-radius: 10px;
+    background-color: green;
+    color: white;
+    border: 2px solid blueviolet;
+  }
+
+  .greetingButton:focus {
+    outline: none;
+  }
+</style>
