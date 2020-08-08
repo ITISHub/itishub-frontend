3c78b8610d9a7fb2269307a7e43533b794e30851
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-08-09 00:36:26 +0300

    added default about page

---
+++ itis-hub/components/DeveloperCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="developer-content">
+    <img :src="require(`~/assets/card-images/developers/${image}`)" alt="developer" class="developer-image">
+    <div class="developer-info">
+      <h4 class="developer-name"> {{ name }} </h4>
+      <p> {{ duties }} </p>
+      <div class="links mt-n3">
+        <v-btn icon target="_blank" :href="github">
+          <img src="~assets/icons/github.svg" alt="github" height="32">
+        </v-btn>
+        <v-btn icon target="_blank" :href="telegram">
+          <img src="~assets/icons/telegram.svg" alt="telegram" height="30">
+        </v-btn>
+        <v-btn icon target="_blank" :href="vk">
+          <img src="~assets/icons/vk.svg" alt="vk" height="30">
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "DeveloperCard",
+    props: {
+      image: String,
+      name: String,
+      duties: String,
+      vk: String,
+      github: String,
+      telegram: String,
+    },
+}
+</script>
+
+<style scoped>
+  .developer-image {
+    max-height: 100px;
+    max-width: 100px;
+    border-radius: 15px;
+  }
+
+  .developer-content {
+    max-width: 300px;
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    text-align: left;
+  }
+
+  .links button {
+    margin-right: -4px;
+  }
+
+  .developer-name {
+    white-space: nowrap;
+  }
+</style>
--- itis-hub/pages/about.vue
+++ itis-hub/pages/about.vue
@@ -1,21 +1,63 @@
 <template>
   <div class="about-wrapper">
-    <div>
-      <h3>About page</h3>
+    <v-container class="greeting">
+      <h2 class="mb-3">Разработчики</h2>
+      <p>thx</p>
+    </v-container>
+    <div class="developers">
+      <DeveloperCard
+        v-for="developer in developers"
+        :key=developer.name
+        :name=developer.name
+        :duties=developer.duties
+        :image=developer.image
+        :github=developer.github
+        :telegram=developer.telegram
+        :vk=developer.vk
+      />
     </div>
   </div>
 </template>
 
 <script>
+    import DeveloperCard from "../components/DeveloperCard";
     export default {
-        name: "about"
+        name: "about",
+      components: {DeveloperCard},
+      data () {
+          return {
+            developers: [
+              {
+                name: 'Elon Musk',
+                image: 'marat.jpg',
+                duties: 'content',
+                github: 'https://github.com/',
+                telegram: 'https://telegram.org/',
+                vk: 'https://vk.com/'
+              },
+            ]
+          }
+        }
     }
 </script>
 
 <style scoped>
-.about-wrapper {
-  display: grid;
-  place-items: center;
-  height: 100vh;
-}
+  .about-wrapper {
+    margin: 0 auto;
+    max-width: 700px;
+    text-align: center;
+  }
+
+  .developers {
+    min-height: 400px;
+    padding: 15px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-column-gap: 15px;
+    justify-items: center;
+  }
+
+  .developers div {
+    margin-bottom: 20px;
+  }
 </style>
