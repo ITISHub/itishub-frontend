d4199cdcd381b6c538c98d4d94cad41a3da95c87
Dan Trofimov <trofimovdanil946@gmail.com>
2022-05-23 02:28:36 +0300

    deploy.sh fixes

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,11 @@ echo 'itis-hub.ru' > CNAME
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+timestamp=$(date + '%H:%M %m/%d/%Y')
+
 git init
 git add -A
-git commit -m "deploy from ${date + '%H:%M %m/%d/%Y'}"
+git commit -m "deploy from ${timestamp}"
 
 # push dist to gh-pages
 git push -f git@github.com:ITISHub/itishub-frontend.git master:gh-pages
