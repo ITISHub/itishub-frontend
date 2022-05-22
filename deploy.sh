c62055a79fe1d9282c478a701e8444d7c7706f2c
Dan Trofimov <trofimovdanil946@gmail.com>
2022-05-22 18:13:48 +0300

    update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# build Nuxt app
+npm run generate
+
+# navigate into the build output directory
+cd dist
+
+# custom domain
+echo 'itis-hub.ru' > CNAME
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy message'
+
+# push dist to gh-pages
+git push -f git@github.com:ITISHub/itishub-frontend.git master:gh-pages
+
+cd -
