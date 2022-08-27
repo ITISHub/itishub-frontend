9bc99dd304a8b79cd82b66327d52119cdfaedff9
DanTrofimov <DanTrofimov@users.noreply.github.com>
2022-08-27 22:31:29 +0000

    deploy: 0b214c4b440d68bb262ee006599df2c1f4391d19

--- _nuxt/static/1661636399/discrete-math-theory/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/discrete-math-theory", {data:[{}],fetch:[{_error:{message:"invalid json response body at https:\u002F\u002Fitishub.pythonanywhere.com\u002Fapi\u002Fv1\u002Fcourses\u002F2 reason: Unexpected token \u003C in JSON at position 0",type:"invalid-json",statusCode:500}}],mutations:[]});
