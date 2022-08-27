8abb480ffc8a1278e239c9598e5759ef2b2c0d8b
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2022-08-28 00:00:06 +0300

    deploy from Sat Aug 27 21:00:06 UTC 2022

---
+++ _nuxt/static/1661634001/about/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/about", {data:[{}],fetch:[{_error:{message:"invalid json response body at https:\u002F\u002Fitishub.pythonanywhere.com\u002Fapi\u002Fv1\u002Fcreators\u002F reason: Unexpected token \u003C in JSON at position 0",type:"invalid-json",statusCode:500}}],mutations:[]});
