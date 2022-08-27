6319082c77d9ecbf95f2d0601c6df88f807bf8a9
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2022-08-28 00:04:15 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,16 @@ npm run generate
 cd dist
 
 # custom domain
-echo 'itis-hub.ru' > CNAME
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# echo 'itis-hub.ru' > CNAME
 
 git init
 git add -A
-git commit -m "deploy from ${date + '%H:%M %m/%d/%Y'}"
+
+message="deploy from "$(date -u)
+
+git commit -m "$message"
 
 # push dist to gh-pages
 git push -f git@github.com:ITISHub/itishub-frontend.git master:gh-pages
 
-cd -
+cd /
