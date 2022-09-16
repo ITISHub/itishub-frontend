1428aeaddd2bbcd770fc322b5898feeea9f597a3
Dan Trofimov <daniil.trofimov@sbermarket.ru>
2022-09-16 18:59:49 +0300

    restore cache

--- store/creators.js
+++ store/creators.js
@@ -14,6 +14,7 @@ export const actions = {
       process.env.baseUrl + process.env.creatorsAccess
     );
     let creators = await response.json();
+    console.log(creators)
     commit("setCreators", creators);
   },
 };
